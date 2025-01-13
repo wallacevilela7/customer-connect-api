2f7f1306637e708521e8c35e0bba8030e6deef62
Wallace Vilela <wallace.vilela9@gmail.com>
2025-01-13 19:50:13 -0300

    Create app

---
+++ src/main/java/wvsdev/costumerconnectapi/CostumerConnectApiApplication.java
@@ -0,0 +1,13 @@
+package wvsdev.costumerconnectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerConnectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CostumerConnectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wvsdev/costumerconnectapi/CostumerConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package wvsdev.costumerconnectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerConnectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
